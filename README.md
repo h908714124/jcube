9b2d35187b4d988366bd236d062d7c10ffa0adc5
jbock <jbock-java@gmx.de>
2022-07-11 10:21:50 +0200

    swap a and c

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ and
 $\omega^3 - \omega^6 = 1$
 . Then
 
-$$\displaylines{a := \omega - \omega^8 \approx 1.8793852415718169\\
+$$\displaylines{a := \omega^7 - \omega^2 \approx -1.5320888862379558}\\
 b := \omega^5 - \omega^4 \approx -0.3472963553338607\\
-c := \omega^7 - \omega^2 \approx -1.5320888862379558}$$
+c := \omega - \omega^8 \approx 1.8793852415718169$$
 
 are the solutions of $x^3 − 3x = 1$. There are three algebraic relations between these numbers:
 
-$$\displaylines{a^2 + c = 2 \\
-b^2 + a = 2 \\
-c^2 + b = 2}$$
+$$\displaylines{a^2 + b = 2 \\
+b^2 + c = 2 \\
+c^2 + a = 2}$$
--- src/test/java/awesome/cubics/PolynomialTest.java
+++ src/test/java/awesome/cubics/PolynomialTest.java
@@ -9,27 +9,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PolynomialTest {
 
-    private final Binomial a = binomial(monomial(1, 1), monomial(-1, 8));
+    private final Binomial a = binomial(monomial(1, 7), monomial(-1, 2));
     private final Binomial b = binomial(monomial(1, 5), monomial(-1, 4));
-    private final Binomial c = binomial(monomial(1, 7), monomial(-1, 2));
+    private final Binomial c = binomial(monomial(1, 1), monomial(-1, 8));
 
     private final Polynomial one = binomial(monomial(1, 3), monomial(-1, 6)).asPolynomial();
 
     @Test
     void testPoly() {
-        Polynomial polynomial = a.pow(5);
+        Polynomial polynomial = c.pow(5);
         assertEquals("- 10r^8 - 5r^6 + r^5 - r^4 + 5r^3 + 10r", polynomial.powermodFlip(9).toString());
     }
 
     @Test
     void testPow1() {
-        Polynomial polynomial = a.pow(1);
+        Polynomial polynomial = c.pow(1);
         assertEquals("- r^8 + r", polynomial.toString());
     }
 
     @Test
     void testPow3() {
-        Polynomial polynomial = a.pow(3);
+        Polynomial polynomial = c.pow(3);
         assertEquals("- 3r^8 - r^6 + r^3 + 3r", polynomial.powermodFlip(9).toString());
     }
 
@@ -43,12 +43,12 @@ class PolynomialTest {
     @Test
     void testRelations() {
         Polynomial constant2 = constant(2);
-        // a^2 + c = 2
-        assertEquals(constant2, a.pow(2).add(c).powermodFlip(9));
-        // b^2 + a = 2
-        assertEquals(constant2, b.pow(2).add(a).powermodFlip(9));
-        // c^2 + b = 2
-        assertEquals(constant2, c.pow(2).add(b).powermodFlip(9));
+        // a^2 + b = 2
+        assertEquals(constant2, a.pow(2).add(b).powermodFlip(9));
+        // b^2 + c = 2
+        assertEquals(constant2, b.pow(2).add(c).powermodFlip(9));
+        // c^2 + a = 2
+        assertEquals(constant2, c.pow(2).add(a).powermodFlip(9));
     }
 
     // compute r^3 - 3r, where r is an 18th root of unity
