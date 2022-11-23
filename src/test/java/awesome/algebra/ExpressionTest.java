5cc50731a7f122ae95dd72c669ec2f5bf0a56ffc
jbock <jbock-java@gmx.de>
2022-11-23 17:32:56 +0100

    update

--- src/test/java/awesome/algebra/ExpressionTest.java
+++ src/test/java/awesome/algebra/ExpressionTest.java
@@ -2,13 +2,15 @@ package awesome.algebra;
 
 import org.junit.jupiter.api.Test;
 
+import static awesome.algebra.Expression.nthRoot;
+import static awesome.algebra.Expression.numeral;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExpressionTest {
 
     @Test
-    void nthRoot() {
-        Root r = Expression.nthRoot(2).of(3);
-        assertEquals(Expression.numeral(3), r.mult(r));
+    void multiplyRoots() {
+        Root r = nthRoot(2).of(3);
+        assertEquals(numeral(3), r.mult(r));
     }
 }
